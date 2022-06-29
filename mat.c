f5e11f4a3f3a08e0b9b3c5771e1519c6b972d094
José Reis <84919858+Xumador@users.noreply.github.com>
2022-06-29 17:19:25 -0300

    Create mat.c

---
+++ mat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    double d;
+    d = sqrt(100);
+        printf("d = %.2lf\n", d);
+    d = sin(45);
+        printf("d = %.2lf\n", d);
+    d = pow(100, 2);
+        printf("d = %.2lf\n", d);
+    return 0;
+}
