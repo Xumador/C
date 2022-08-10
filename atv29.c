6ceee423118dab8d4a8928abaf49954e4c849f7b
José Reis <84919858+Xumador@users.noreply.github.com>
2022-08-10 17:06:44 -0300

    Add files via upload

---
+++ atv29.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  
+  int qtd, *valores;
+  float mediana;
+
+  puts("Digite a quantidade e os valores: ");
+  scanf("%d", &qtd);
+  valores = malloc(qtd * sizeof(int));
+  
+  printf("Insira os %d valores: \n", qtd);
+
+  for (int i = 0; i < qtd; i++) {
+    scanf("%d", &valores[i]);
+  }
+  
+  if (qtd % 2 != 0)
+    mediana = valores[((qtd + 1) / 2) - 1];
+  else
+    mediana = (valores[(qtd) / 2] + valores[((qtd / 2) - 1)]) / 2;
+  
+  for (int i = 0; i < qtd; i++) {
+    printf("%d ", valores[i]);
+  }
+  
+  printf("\n");
+  
+  printf("A mediana dos valores é: %.2f\n", mediana);
+
+  free(valores);
+
+  return 0;
+}
